78b0bf38f4a47b7d57f41ad7a492b9ec1d87b45d
unknown <rmasoumvand@yahoo.com>
2022-08-13 15:40:54 +0430

    Updated settings.py

--- eshop_project/settings.py
+++ eshop_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d#^t$cbltd$e(+hpork231&^uv@_l8!13_bt^=lqsetoud^_0^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # internal apps
     'home_module',
     'product_module',
     'contact_module',
