fb4da2d4c0b2a7b838745156807bce34c48aae68
unknown <rmasoumvand@yahoo.com>
2022-08-30 16:24:54 +0430

    Updated payment_module

--- apppayment/apps.py
+++ apppayment/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ApppaymentConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apppayment'
+    verbose_name =  'تراکنش های بانکی'
